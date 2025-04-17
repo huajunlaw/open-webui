9b62b5fa95d520f3a867948f8d46253d1c421b78
yhllm2025 <2676172942@qq.com>
2025-04-17 15:26:55 +0800

    change logo

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -528,7 +528,7 @@
 					<div class="self-center mx-1.5">
 						<img
 							crossorigin="anonymous"
-							src="{WEBUI_BASE_URL}/static/favicon.png"
+							src="/huajun-logo.png"
 							class=" size-5 -translate-x-1.5 rounded-full"
 							alt="logo"
 						/>
